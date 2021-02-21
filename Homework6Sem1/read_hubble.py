2022a142aef2a66dbfea1ec5e1ecec372333729b
chancestarr <chance@buffalo.edu>
2021-02-21 08:55:11 -0500

    Chance's Update

---
+++ Homework6Sem1/read_hubble.py
@@ -0,0 +1,21 @@
+from numpy import genfromtxt
+
+def read_hubble(file_name = "Hubble.txt", verbose=False):
+    '''
+    # from https://www.esrl.noaa.gov/gmd/ccgg/trends/
+    # CO2 expressed as a mole fraction in dry air, micromol/mol, abbreviated as ppm
+    #
+    #  (-99.99 missing data;  -1 no data for #daily means in month)
+    #
+    #            decimal     average   interpolated    trend    #days
+    #             date                             (season corr)
+    '''
+
+    data = genfromtxt(file_name, delimiter=",")
+
+    distance = data[:,0]
+    velocity = data[:,1]
+    if verbose:
+        print (dates)
+        print(averages)
+    return [distance, velocity]
